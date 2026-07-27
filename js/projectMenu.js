a8d9aa72247542e83c45166ff5397e95b077133a
LionTurtle-dev <patakitasbalint@gmail.com>
2026-07-28 01:14:47 +0200

    JavaScript kód felkészítve a 'click' eseményekre.(Új project, Project-ek listázása, Project keresése)

--- js/projectMenu.js
+++ js/projectMenu.js
@@ -39,6 +39,36 @@ const DASHBOARD = `
     </div>
 </menu>
 `;
+const NEW_PROJECT_CONT = `
+<form action="">
+    <fieldset>
+        <legend><strong>Új Project</strong></legend>
+        <label for="name">Project Neve*:</label>
+        <input title="Adj egy frappáns nevet a project-ednek!" placeholder="Új Project" id="name" type="text" required autofocus><br>
+        <label for="description">Project Leírása</label><br>
+        <textarea title="Adj egy rövid leírást a project-ről, hogy könnyebben átlásd, mit kell csinálnod!" placeholder="Project rövid Leírása" id="description" cols="45" rows="10"></textarea><br>
+    </fieldset>
+    <div id="buttons">
+        <button type="submit" id="submit">Project létrehozása</button>
+        <button id="cancel">Mégse</button>
+    </div>
+</form>
+`;
+
+function new_project() {
+    // Új project űrlapja
+    console.log('Új Project');
+}
+
+function list_projects() {
+    // Project-ek listázása
+    console.log('Listázás');
+}
+
+function seek_project(seeker) {
+    // Project keresése
+    console.log(seeker.value);
+}
 
 const myHeaders = new Headers();
 myHeaders.set('Mission', 'Get-Projects-Menu')
@@ -51,6 +81,12 @@ fetch('http://localhost:3333/data/projects.json', myHeaders)
         MN.innerHTML = MAIN_MENU;
         const NEW_PROJECT   = document.getElementById('new-project');
         const LIST_PROJECTS = document.getElementById('list-projects');
+        const SEEK_PROJECT  = document.getElementById('seek-project'); //Input mező
+        const START_SEEKING = document.getElementById('start-seek-project');
+
+        NEW_PROJECT.addEventListener('click', () => {new_project();});
+        LIST_PROJECTS.addEventListener('click', () => {list_projects();});
+        START_SEEKING.addEventListener('click', () => {seek_project(SEEK_PROJECT);});
     })
     .catch((reas) => {
         HEAD.innerHTML = `<h1>Nem sikerült betölteni az adatokat.</h1>`;
